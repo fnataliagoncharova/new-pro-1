090247e92d3c11c56d4c1543019ccb638fb932a7
Goncharova Natalia <gnatalya444@gmail.com>
2022-08-24 20:09:45 +0700

    first_commit

---
+++ tab.py
@@ -0,0 +1,12 @@
+for col in range (1, 10):
+  print(end='\n')
+  for row in range(1,10):
+    print(row * col, end='\t')
+
+
+
+
+
+print("выводим таблицу умножения !")
+print("выводим таблицу умножения !")
+print("выводим таблицу умножения !")
